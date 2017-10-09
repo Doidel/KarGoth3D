c805775df04808908934dd92158f5d5f9316fdb5
Doidel <romanbolzern@hotmail.com>
2017-10-09 19:15:42 +0200

    wheat beginning

---
+++ Assets/Scripts/WheatField.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheatField : MonoBehaviour {
+
+    public GameObject WheatSprite;
+
+	// Use this for initialization
+	void Start () {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                var sprite = Instantiate(WheatSprite);
+                //sprite.transform.pos
+                sprite.transform.SetParent(transform);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
