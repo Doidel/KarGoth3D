672de618fad4b0b125e61a5bf1f7362a184df661
Doidel <romanbolzern@hotmail.com>
2017-10-09 22:02:50 +0200

    meshcombiner panics

---
+++ Assets/Scripts/Editor/MeshCombinerEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(hMeshCombiner))]
+public class MeshCombinerEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        hMeshCombiner mc = hMeshCombiner.Instance;
+        if (Handles.Button(mc.transform.position+Vector3.up * 5, Quaternion.LookRotation(Vector3.up), 1, 1, Handles.CylinderCap))
+        {
+            var t = (target as hMeshCombiner).gameObject;
+            mc.Combine(t, t.transform, false);
+        }
+    }
+}
